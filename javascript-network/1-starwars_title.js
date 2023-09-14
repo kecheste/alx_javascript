c3b2fa4a3588fabf0575d760745c858da194fb7e
kecheste <aben.ezer443@gmail.com>
2023-09-14 10:42:57 +0300

    alx javascript network all

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,13 @@
+const request = require("request");
+const args = process.argv.slice(2);
+// slice(2) === [2:] in python
+
+request(
+  "https://swapi-api.alx-tools.com/api/films/" + args[0],
+  (error, response, body) => {
+    // Convert the string to JSON
+    const jsonObject = JSON.parse(body);
+
+    console.log(jsonObject.title);
+  }
+);
---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,33 @@
+const request = require("request");
+
+const url = process.argv[2]; // URL to request
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error("Error:", error);
+  } else if (response.statusCode !== 200) {
+    console.error("Status:", response.statusCode);
+  } else {
+    const jsonObject = JSON.parse(body);
+    const filtered = jsonObject.filter(
+      (jsonObject) => jsonObject.completed === true
+    );
+    console.log(countElements(filtered.map((ele, index) => ele.userId)));
+  }
+});
+
+function countElements(list) {
+  let counter = {};
+
+  for (let i = 0; i < list.length; i++) {
+    let element = list[i];
+
+    if (counter[element]) {
+      counter[element]++;
+    } else {
+      counter[element] = 1;
+    }
+  }
+
+  return counter;
+}
